d6ce27ae8c0f9c458a9f7ec5113ad5ddf5329b59
Ruben Sanchez <rubensanchezcolin@gmail.com>
2020-08-29 00:54:46 -0500

    first commit

---
+++ invertirCadena.js
@@ -0,0 +1,13 @@
+const inputText = () => {
+    let cadena = (document.forms[0].cadena.value);
+    alert (invertirFrase(cadena));
+} 
+
+const invertirFrase = (cadena) => (cadena.includes( '(') ) ? invertirFrase( invertirParentesis(cadena) ) : cadena.split("").reverse().join("");
+
+function invertirParentesis(cadena){
+    let expRegular = /\(([^()]*)\)/i;
+    let subCadena = expRegular.exec(cadena)[1];
+    subCadena = subCadena.split('').reverse().join('');
+    return cadena.replace(expRegular, subCadena)
+}
