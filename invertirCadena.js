125252c729e0da6b0a201f57d6696e291fd37f80
Ruben Sanchez <rubensanchezcolin@gmail.com>
2020-08-29 01:06:07 -0500

    DOM value default

--- invertirCadena.js
+++ invertirCadena.js
@@ -3,11 +3,14 @@ const inputText = () => {
     alert (invertirFrase(cadena));
 } 
 
-const invertirFrase = (cadena) => (cadena.includes( '(') ) ? invertirFrase( invertirParentesis(cadena) ) : cadena.split("").reverse().join("");
+const invertirFrase = (cadena) => (cadena.includes( '(') ) ? invertirFrase( invertirParentesis(cadena) ) : cadena;
 
 function invertirParentesis(cadena){
+    alert (cadena);
     let expRegular = /\(([^()]*)\)/i;
     let subCadena = expRegular.exec(cadena)[1];
     subCadena = subCadena.split('').reverse().join('');
     return cadena.replace(expRegular, subCadena)
 }
+
+document.getElementById('cadena').value = '(Hola (Mundo))';
